d7e5aa09965fb1d0078eb1d6cb176575161b84f9
Raviraj Bhalerao <raviraj.bhalerao@gmail.com>
2026-03-08 18:40:31 +0100

    Update test command in GitHub Actions and enable benchmark tests in Program.cs

--- src/Samwin.UmlautConverter.Benchmarks/Program.cs
+++ src/Samwin.UmlautConverter.Benchmarks/Program.cs
@@ -5,8 +5,8 @@ class Program
     static void Main(string[] args)
     {
         // Runs all benchmarks in the UmlautConverterBenchmark class
-        // var summaryStep1Names = BenchmarkRunner.Run<UmlautConverterNamesBenchmark>();
-        // var summaryStep1LongText = BenchmarkRunner.Run<UmlautConverterLongTextBenchmark>();
+        var summaryStep1Names = BenchmarkRunner.Run<UmlautConverterNamesBenchmark>();
+        var summaryStep1LongText = BenchmarkRunner.Run<UmlautConverterLongTextBenchmark>();
         var summaryStep2 = BenchmarkRunner.Run<VariationConverterBenchmark>();
     }
 }
